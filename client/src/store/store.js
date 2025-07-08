6780581561b96a58b045a2e277b0c87a195217a8
Ashish21212 <acp.aashish44@gmail.com>
2025-07-09 00:13:55 +0545

    backend fetches from redux toolkit

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,7 @@ import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
 import shopProductSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
+import shopAddressSlice from "./shop/address-slice";
 
 
 
@@ -12,6 +13,7 @@ const store = configureStore({
      adminProducts:adminProductsSlice,
      shopProducts:shopProductSlice,
      shopCart:shopCartSlice,
+     shopAddress:shopAddressSlice,
   }
 })
 export default store;
